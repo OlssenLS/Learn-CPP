7374adf314c7d4fea4c05e426cbfbdf518dfaea4
NexusCycle <117983286+NexusCycle@users.noreply.github.com>
2022-11-13 19:49:27 +0700

    Upload assignment 4 & 5

---
+++ Assignment Program/ass-4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World\n";
+    cout << "\nNama Anda\nAsal Sekolah\n";
+    cout << "\nPemrograman dan Algoritma\n";
+
+    return 0;
+}
---
+++ Assignment Program/ass-5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int integer;
+
+    cout << "Masukkan Integer: ";
+    cin >> integer;
+    
+    cout << "Integer: " << integer;
+    cout << "\n--------------------------------\n";
+
+    return 0;
+}
